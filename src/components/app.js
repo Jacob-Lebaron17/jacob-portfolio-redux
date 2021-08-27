6ec6d4606fe96750b34bcc5d9a5fcee10fe320b7
jacobftp <jacobolebaron@gmail.com>
2021-08-27 17:13:03 -0600

    second commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,19 +3,25 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrash,
+  faSignOutAlt,
+  faEdit,
+  faCannabis
+} from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt);
+library.add(faTrash, faSignOutAlt, faEdit);
 
 export default class App extends Component {
   constructor(props) {
@@ -115,6 +121,7 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              <Route path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN" ? (
                 this.authorizedPages()
               ) : null}
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 import { Link } from "react-router-dom";
+class Blog extends Component {
+  constructor() {
+    super();
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
+    this.state = {
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
+      isLoading: false
+    };
 
-      <div>
-        <Link to="/about-me">Read more about myself</Link>
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.activateInfiniteScroll();
+  }
+
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        console.log("get more posts");
+      }
+    };
+  }
+
+  getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => { 
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records,
+          isLoading: false
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
+
+    return (
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
+    
+        {this.state.isLoading ? (
+        <div className="content-loader">
+          <FontAwesomeIcon icon="Cannabis" spin />
+        </div>) : null}
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Blog;
